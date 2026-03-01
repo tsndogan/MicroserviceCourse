c837c71e5926ec4cd334ab85b241d050a0a063a8
tsndogan <dogantsn61@gmail.com>
2026-03-02 01:47:20 +0300

    Fluent validation and automapper used.

---
+++ UdemyNewMicroservice.Catalog.Api/Features/Categories/Create/CreateCategoryCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace UdemyNewMicroservice.Catalog.Api.Features.Categories.Create
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("{Property Name} cannot be empty")
+                .Length(4, 25).WithMessage("{Property Name} must be between 4 and 25 characters");
+        }
+    }
+}
